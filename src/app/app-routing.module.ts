8f674f9bb0d72601d393b70e154d6351fb6915dc
tzmanics <tzmanics@gmail.com>
2019-07-08 23:55:33 -0400

    adds routes and new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page-one', component: PageOneComponent },
+  { path: 'page/:id', component: PageDetailComponent },
+  { path: 'pages', component: PageListComponent, data: { title: 'Page' }},
+  { path: '', redirectTo: '/pages', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageDetailComponent } from './page-detail/page-detail.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageDetailComponent,
+    PageListComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
