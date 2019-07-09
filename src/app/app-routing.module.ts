be27907933f131470fd9003c27c9e496e30c4927
tzmanics <tzmanics@gmail.com>
2019-07-09 00:15:17 -0400

    adds route links, imports components & adds netlify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageDetailComponent } from './page-detail/page-detail.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 const routes: Routes = [
   { path: 'page-one', component: PageOneComponent },
